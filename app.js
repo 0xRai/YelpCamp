894be099dfccb8f788c6c151e8047fac667d5338
Rai <robertblake.hardy@outlook.com>
2022-02-12 23:45:34 -0100

    ready for deploy

--- app.js
+++ app.js
@@ -15,14 +15,20 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
+const MongoDBStore = require('connect-mongo')(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 const port = 3000;
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -41,13 +47,32 @@ app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+
+const secret = process.env.SECRET || secrettest;
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret: secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e) {
+    console.log("Session store error", e)
+})
+
 
 const sessionConfig = {
-    secret: 'secrettest',
+    store,
+    name: 'session',
+    secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -56,16 +81,63 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dqdnu1b6g/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dqdnu1b6g/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    "https://res.cloudinary.com/dqdnu1b6g"
+];
+const fontSrcUrls = ["https://res.cloudinary.com/dqdnu1b6g/"];
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dqdnu1b6g/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+            mediaSrc: ["https://res.cloudinary.com/dqdnu1b6g/"],
+            childSrc: ["blob:"]
+        }
+    })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
@@ -77,6 +149,10 @@ app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 
 app.all('*', (req, res, next) => {
--- seeds/index.js
+++ seeds/index.js
@@ -20,16 +20,31 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
             author: '62042fa534c495321108dc29',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [{
+                    url: "https://res.cloudinary.com/dqdnu1b6g/image/upload/v1644614114/YelpCamp/wdyd0qemqr8zkfunncu1.png",
+                    filename: "YelpCamp/wdyd0qemqr8zkfunncu1"
+                },
+                {
+                    url: "https://res.cloudinary.com/dqdnu1b6g/image/upload/v1644614114/YelpCamp/l1daig0ohjzk6ggoda6m.png",
+                    filename: "YelpCamp/l1daig0ohjzk6ggoda6m"
+                }
+            ],
             description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nemo adipisci architecto, amet cumque vel. Repellat ea quas ut, odio recusandae pariatur rem tenetur iusto placeat. Quas voluptatibus enim dolore.',
-            price
+            price,
+            geometry: {
+                "type": "Point",
+                "coordinates": [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            }
         })
         await camp.save();
     }
